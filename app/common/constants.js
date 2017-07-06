b73a1135ef059f2f42dd3d5c9363864a397feb61
David Alecrim <david.alecrim1@gmail.com>
2017-07-06 14:40:05 +0100

    no message

--- app/common/constants.js
+++ app/common/constants.js
@@ -1,7 +1,7 @@
+export const API_ENDPOINT = 'http://192.168.1.30:8080';
 export const LOG_USER = 'log_user';
 export const LOGOUT_USER = 'logout_user';
 export const REGISTER_USER = 'register_user';
-export const API_ENDPOINT = 'http://192.168.1.30:8080';
 export const ROUTES = {
     BASE: '/',
     LOGIN: '/login',
