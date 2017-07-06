1ad95ca2df5e53a73514ec55ba3e0b94f637ab09
David Alecrim <david.alecrim1@gmail.com>
2017-07-06 17:52:44 +0100

    added cards with MASA test to the new evaluation component

--- app/components/new_evaluation.js
+++ app/components/new_evaluation.js
@@ -1,12 +1,47 @@
 import React from 'react';
-import { Row, Col } from 'reactstrap';
+import { Row, Col, Button, Card, CardImg, CardText, CardBlock,
+    CardTitle } from 'reactstrap';
 
 const NewEvaluation = () => {
     return (
-        <div className="content text-center">
+        <div className="content">
             <Row>
                 <Col xs="12">
-                    <h1>New Evaluation</h1>
+                    <h1 className="push-down">New Evaluation</h1>
+                    <Row>
+                        <Col xs="4">
+                            <Card>
+                                <CardImg
+                                    top
+                                    width="100%"
+                                    src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
+                                    alt="Card image cap" />
+                                <CardBlock>
+                                    <CardTitle>MASA</CardTitle>
+                                    <CardText>
+                                        MASA test to quickly evaluate the state of the patient relating to deglutation and other parameters.
+                                    </CardText>
+                                    <Button color="primary" outline>Start test</Button>
+                                </CardBlock>
+                            </Card>
+                        </Col>
+                        <Col xs="4">
+                            <Card>
+                                <CardImg
+                                    top
+                                    width="100%"
+                                    src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
+                                    alt="Card image cap" />
+                                <CardBlock>
+                                    <CardTitle>Other tests</CardTitle>
+                                    <CardText>
+                                        Other tests with custom parameters are coming soon to this platform. Stay tuned.
+                                    </CardText>
+                                    <Button color="primary" outline disabled>Start test</Button>
+                                </CardBlock>
+                            </Card>
+                        </Col>
+                    </Row>
                 </Col>
             </Row>
         </div>
