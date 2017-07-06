cab4d7f1580e86e5c5720075db753e36dd5ed855
David Alecrim <david.alecrim1@gmail.com>
2017-07-06 17:16:31 +0100

    updated production webpack.config

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -5,66 +5,69 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const config = {
-  devtool: 'cheap-module-source-map',
+    devtool: 'cheap-module-source-map',
 
-  entry: [
-    './main.js',
-    './assets/scss/main.scss',
-  ],
+    entry: [
+        './main.js',
+        './assets/scss/main.scss',
+    ],
 
-  context: resolve(__dirname, 'app'),
+    context: resolve(__dirname, 'app'),
 
-  output: {
-    filename: 'bundle.js',
-    path: resolve(__dirname, 'dist'),
-    publicPath: '',
-  },
+    output: {
+        filename: 'bundle.js',
+        path: resolve(__dirname, 'dist'),
+        publicPath: '',
+    },
 
-  plugins: [
-    new webpack.optimize.ModuleConcatenationPlugin(),
-    new HtmlWebpackPlugin({
-      template: `${__dirname}/app/index.html`,
-      filename: 'index.html',
-      inject: 'body',
-    }),
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-      debug: false,
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      beautify: false
-    }),
-    new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('production') } }),
-    new ExtractTextPlugin({ filename: 'style.css', disable: false, allChunks: true }),
-    new CopyWebpackPlugin([{ from: './vendors', to: 'vendors' }]),
-  ],
-
-  module: {
-    loaders: [
-      {
-        test: /\.js?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.scss$/,
-        exclude: /node_modules/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            'css-loader',
-            { loader: 'sass-loader', query: { sourceMap: false } },
-          ],
+    plugins: [
+        new webpack.optimize.ModuleConcatenationPlugin(),
+        new HtmlWebpackPlugin({
+            template: `${__dirname}/app/index.html`,
+            filename: 'index.html',
+            inject: 'body',
         }),
-      },
-      { test: /\.(png|jpg)$/, use: 'url-loader?limit=15000' },
-      { test: /\.eot(\?v=\d+.\d+.\d+)?$/, use: 'file-loader' },
-      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff' },
-      { test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/octet-stream' },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=image/svg+xml' },
-    ]
-  },
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true,
+            debug: false,
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            beautify: false,
+        }),
+        new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('production') } }),
+        new ExtractTextPlugin({ filename: 'style.css', disable: false, allChunks: true }),
+    ],
+
+    module: {
+        loaders: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        'css-loader',
+                        { loader: 'sass-loader', query: { sourceMap: false } },
+                    ],
+                }),
+            },
+            { test: /\.(png|jpg)$/, use: 'url-loader?limit=15000' },
+            { test: /\.eot(\?v=\d+.\d+.\d+)?$/, use: 'file-loader' },
+            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff' },
+            { test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/octet-stream' },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=image/svg+xml' },
+        ],
+    },
 };
 
 module.exports = config;
