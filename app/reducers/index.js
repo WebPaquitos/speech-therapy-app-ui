afadce1726c75edbdf57c941d57bb26b3d44ed5b
David Alecrim <david.alecrim1@gmail.com>
2017-07-14 15:52:49 +0100

    clear redux store in case of a logout

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -4,13 +4,22 @@ import SessionReducer from './reducer_session';
 import HistoryReducer from './reducer_history';
 import PatientsReducer from './reducer_patients';
 import MASAReducer from './reducer_masa';
+import { LOGOUT_USER } from '../common/constants';
 
-const rootReducer = combineReducers({
+const appReducer = combineReducers({
     session: SessionReducer,
     history: HistoryReducer,
     patients: PatientsReducer,
     masa: MASAReducer,
     form: FormReducer,
 });
+
+const rootReducer = (state, action) => {
+    let stateRef = state;
+    if (action.type === LOGOUT_USER) {
+        stateRef = undefined;
+    }
+    return appReducer(stateRef, action);
+};
 
 export default rootReducer;
