9f927fe518416f86e10eb9378ccf905cc2dbbcb4
David Alecrim <david.alecrim1@gmail.com>
2017-08-18 17:20:03 +0100

    updated dashboard fonts to bootstrap 4-beta elements

--- app/components/dashboard.js
+++ app/components/dashboard.js
@@ -6,11 +6,8 @@ const Dashboard = () => {
         <div className="content text-center">
             <Row>
                 <Col xs="12">
-                    <h1>
-                        Welcome to the Speech Therapy App!
-                        <br/><br/>
-                        <small className="text-muted">Please choose an action from the sidebar</small>
-                    </h1>
+                    <h1 className="display-4 push-down">Welcome to the Speech Therapy App!</h1>
+                    <h3 className="text-muted">Please choose an action from the sidebar</h3>
                 </Col>
             </Row>
         </div>
