a02789428018d23f360a52132172c8a79301b6d7
David Alecrim <david.alecrim1@gmail.com>
2017-08-18 16:59:18 +0100

    task is now done

--- app/containers/history_item.js
+++ app/containers/history_item.js
@@ -1,4 +1,4 @@
-import { Row, Col } from 'reactstrap';
+import { Row, Col, ListGroup, ListGroupItem, ListGroupItemText, ListGroupItemHeading } from 'reactstrap';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchHistoryItem } from '../actions';
@@ -11,14 +11,37 @@ class HistoryItem extends Component {
     render() {
         const { masa } = this.props;
         if (!masa) return <div>Loading...</div>;
+        const patientBirthdate = new Date(masa.patient.birthdate);
         return (
             <div className="content">
                 <Row>
                     <Col xs="12">
                         <h1 className="push-down">History Case #{masa._id}</h1>
                         <Row>
-                            <Col xs="12">
-                                {masa.patient.name}
+                            <Col xs="12" className="push-down">
+                                <h3 className="text-primary"><strong>{masa.patient.id}</strong> - {masa.patient.name}</h3>
+                                <small className="text-muted">
+                                    {`${patientBirthdate.getDate()}/${patientBirthdate.getMonth()}/${patientBirthdate.getFullYear()}`}
+                                </small>
+                                <p className="lead">Additional data: {masa.patient.description}</p>
+                            </Col>
+                            <Col xs="12" className="push-down">
+                                <ListGroup>
+                                    <ListGroupItem active>
+                                        <ListGroupItemHeading>Test results</ListGroupItemHeading>
+                                    </ListGroupItem>
+                                    {masa.fields.map((field) => {
+                                        return (
+                                            <ListGroupItem>
+                                                <ListGroupItemHeading>{field.label}</ListGroupItemHeading>
+                                                <ListGroupItemText>
+                                                    <strong>{field.chosen}</strong>
+                                                    - {field.options.find(({ value }) => value === field.chosen).name}
+                                                </ListGroupItemText>
+                                            </ListGroupItem>
+                                        );
+                                    })}
+                                </ListGroup>
                             </Col>
                         </Row>
                     </Col>
